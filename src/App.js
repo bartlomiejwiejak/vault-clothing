63d343536929ce2923a7047adae448c80ff6cbb3
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-06 21:41:06 +0200

    Homepage basic layout and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+
+import Homepage from './Homepage';
 
 function App() {
   return (
     <div className="App">
-
+      <Homepage />
     </div>
   );
 }
