7da14bf61c9eef116d25eb80f341d029c547cea9
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-07 16:17:52 +0200

    Header component

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './views/Home';
 import Shop from './views/Shop';
+import Header from './layout/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/shop' component={Shop} />
