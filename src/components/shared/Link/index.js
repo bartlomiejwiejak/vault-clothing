81babfab7792e814a9b33f8e45961531e1060094
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-22 18:21:11 +0200

    Link component

---
+++ src/components/shared/Link/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
+import { setAnimating, setPath } from '../../../redux/routing/actions';
+
+const Link = ({ children, to }) => {
+
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const animating = useSelector(state => state.routing.animating)
+
+  function startRedirecting() {
+    if (animating || location.pathname === to) return;
+    dispatch(setPath(to));
+    dispatch(setAnimating(true));
+  }
+
+  return (
+    <div onClick={startRedirecting} style={{ display: 'inline-block', position: 'relative', zIndex: 1 }}>
+      {children}
+    </div>
+  );
+}
+
+export default Link;
