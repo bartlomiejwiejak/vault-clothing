e087fc2c79438e3ed3eabb3b32b2c233fd834d16
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-23 22:37:18 +0200

    Fixed input

--- src/components/shared/Input/index.js
+++ src/components/shared/Input/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-import './styles.scss';
+import {
+  StyledGroup,
+  StyledInput,
+  StyledLabel
+} from './styles';
 
-const Input = ({ handleChange, label, ...otherProps }) => {
+const Input = ({ handleChange, label, ...props }) => {
   return (
-    <div className='group'>
-      <input className='form-input' onChange={handleChange} {...otherProps} />
-      {
-        label ?
-          (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
-            {label}
-          </label>)
-          : null
-      }
-    </div>
+    <StyledGroup>
+      <StyledInput onChange={handleChange} {...props} />
+      {label ? (
+        <StyledLabel className={props.value.length ? 'shrink' : ''}>
+          {label}
+        </StyledLabel>
+      ) : null}
+    </StyledGroup>
   )
 }
 
---
+++ src/components/shared/Input/styles.js
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+const shrinkLabelStyles = css`
+  top: -1.5rem;
+  font-size: 1.4rem;
+  color: var(--color-black);
+`;
+
+export const StyledGroup = styled.div`
+  position: relative;
+  margin: 5rem 0;
+  input[type='password'] {
+    letter-spacing: 0.3em;
+  }
+`;
+
+export const StyledInput = styled.input`
+  background: none;
+  background-color: white;
+  color: var(--color-grey);
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid var(--color-grey);
+  margin: 25px 0;
+  &:focus {
+    outline: none;
+  }
+  &:focus ~ label {
+    ${shrinkLabelStyles}
+  }
+`;
+
+StyledInput.displayName = 'StyledInput';
+
+export const StyledLabel = styled.label`
+  color: var(--color-grey);
+  font-size: var(--default-font-size);
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
+  &.shrink {
+    ${shrinkLabelStyles}
+  }
+`;
+
+StyledLabel.displayName = 'StyledLabel';
