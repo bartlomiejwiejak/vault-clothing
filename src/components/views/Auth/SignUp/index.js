6a9638d767958c725b683b4f8548e284b343e086
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-24 22:16:59 +0200

    SignUp validation

--- src/components/views/Auth/SignUp/index.js
+++ src/components/views/Auth/SignUp/index.js
@@ -1,38 +1,55 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 
 import Input from '../../../shared/Input';
 import Button from '../../../shared/Button';
 import { signUpStart } from '../../../../redux/user/actions';
 import { StyledSignUp, StyledTitle } from './styles';
+import useForm from '../../../../hooks/useForm';
+import { validateMinLength, validateEmail } from '../../../../functions/validators';
 
 const SignUp = ({ signUpStart }) => {
-  const [formState, setFormState] = useState({
-    displayName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+  const { formState, handleChange, handleTouch } = useForm({
+    inputs: {
+      displayName: {
+        value: '',
+        validator: validateMinLength(3),
+        isValid: false,
+        isTouched: false
+      },
+      email: {
+        value: '',
+        validator: validateEmail,
+        isValid: false,
+        isTouched: false
+      },
+      password: {
+        value: '',
+        validator: validateMinLength(6),
+        isValid: false,
+        isTouched: false
+      },
+      confirmPassword: {
+        value: '',
+        validator: validateMinLength(6),
+        isValid: false,
+        isTouched: false
+      }
+    }
   })
 
   async function handleSubmit(e) {
     e.preventDefault()
-    const { displayName, email, password, confirmPassword } = formState;
-    if (password !== confirmPassword) {
-      alert("Passwords don't match")
-      return;
-    }
-    signUpStart(displayName, email, password)
+    const { displayName, email, password } = formState.inputs;
+
+    signUpStart(displayName.value, email.value, password.value)
   }
-  function handleChange(e) {
+  function inputChange(e) {
     const { name, value } = e.target;
-    setFormState(prevState => {
-      return {
-        ...prevState,
-        [name]: value
-      }
-    })
+
+    handleChange(name, value);
   }
-  const { displayName, email, password, confirmPassword } = formState;
+  const { displayName, email, password, confirmPassword } = formState.inputs;
   return (
     <StyledSignUp>
       <StyledTitle>I do not have a account</StyledTitle>
@@ -41,36 +58,52 @@ const SignUp = ({ signUpStart }) => {
         <Input
           type='text'
           name='displayName'
-          value={displayName}
-          onChange={handleChange}
+          value={displayName.value}
+          onChange={inputChange}
+          isValid={displayName.isValid}
+          isTouched={displayName.isTouched}
+          errorMessage='Name should have at least 3 characters.'
+          onBlur={() => handleTouch('displayName')}
           label='Display Name'
           required
         />
         <Input
           type='email'
           name='email'
-          value={email}
-          onChange={handleChange}
+          value={email.value}
+          errorMessage='Please provide a valid email address.'
+          onChange={inputChange}
+          isValid={email.isValid}
+          isTouched={email.isTouched}
+          onBlur={() => handleTouch('email')}
           label='Email'
           required
         />
         <Input
           type='password'
           name='password'
-          value={password}
-          onChange={handleChange}
+          value={password.value}
+          errorMessage='Password should have at least 6 characters.'
+          onChange={inputChange}
+          isValid={password.isValid}
+          isTouched={password.isTouched}
+          onBlur={() => handleTouch('password')}
           label='Password'
           required
         />
         <Input
           type='password'
           name='confirmPassword'
-          value={confirmPassword}
-          onChange={handleChange}
+          value={confirmPassword.value}
+          errorMessage='Passwords don`t match!'
+          onChange={inputChange}
+          isValid={confirmPassword.value === password.value}
+          isTouched={confirmPassword.isTouched}
+          onBlur={() => handleTouch('confirmPassword')}
           label='Confirm Password'
           required
         />
-        <Button type='submit'>SIGN UP</Button>
+        <Button type='submit' disabled={!formState.isFormValid || confirmPassword.value !== password.value}>SIGN UP</Button>
       </form>
     </StyledSignUp>
   )
