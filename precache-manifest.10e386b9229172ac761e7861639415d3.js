7901a30eb31193273471658aa63b9046496c5526
Bartłomiej Wiejak <bartlomiej.wiejak@infrabyte.pl>
2023-08-19 17:33:08 +0200

    Updates

---
+++ precache-manifest.10e386b9229172ac761e7861639415d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d667b9507c062d4b4f706d0c826ced2",
+    "url": "/vault-clothing/index.html"
+  },
+  {
+    "revision": "849c377a456d10b99071",
+    "url": "/vault-clothing/static/css/main.d31ec4d2.chunk.css"
+  },
+  {
+    "revision": "4f217125f41b56eea750",
+    "url": "/vault-clothing/static/js/0.7fdf856d.chunk.js"
+  },
+  {
+    "revision": "2b0e0044848ecd9a10ee7dea63028591",
+    "url": "/vault-clothing/static/js/0.7fdf856d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7910974a6ad8e4686509",
+    "url": "/vault-clothing/static/js/3.f8e50c7e.chunk.js"
+  },
+  {
+    "revision": "f5bbba52c3b12912fe28e5ffe997c808",
+    "url": "/vault-clothing/static/js/3.f8e50c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce943a0bd2171dc9fe57",
+    "url": "/vault-clothing/static/js/4.933d9cfa.chunk.js"
+  },
+  {
+    "revision": "33d7c827170eeaec86a8",
+    "url": "/vault-clothing/static/js/5.ecf300d1.chunk.js"
+  },
+  {
+    "revision": "40f2b87cb039fc071383",
+    "url": "/vault-clothing/static/js/6.9db16b41.chunk.js"
+  },
+  {
+    "revision": "c5e76604c93319c01f98",
+    "url": "/vault-clothing/static/js/7.19cc2393.chunk.js"
+  },
+  {
+    "revision": "b092d38efd239c5c8dac",
+    "url": "/vault-clothing/static/js/8.7aec8a6c.chunk.js"
+  },
+  {
+    "revision": "71ffd7d700d7efd151fe",
+    "url": "/vault-clothing/static/js/9.3993b7fc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/vault-clothing/static/js/9.3993b7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849c377a456d10b99071",
+    "url": "/vault-clothing/static/js/main.31e96e54.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/vault-clothing/static/js/main.31e96e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f899bba4d36e30c663",
+    "url": "/vault-clothing/static/js/runtime-main.826ddfc8.js"
+  },
+  {
+    "revision": "0572e2ad614e02f439fe939004929045",
+    "url": "/vault-clothing/static/media/about-1.0572e2ad.jpg"
+  },
+  {
+    "revision": "f3a2b21d1842bcb2c5c3fa2afa2a4ef4",
+    "url": "/vault-clothing/static/media/about-2.f3a2b21d.jpg"
+  },
+  {
+    "revision": "7ebbc0b87d3960f74cc145d2a0d296a4",
+    "url": "/vault-clothing/static/media/about-3.7ebbc0b8.jpg"
+  },
+  {
+    "revision": "074e490e2b3b211a25bc4402cff6a339",
+    "url": "/vault-clothing/static/media/about-4.074e490e.jpg"
+  },
+  {
+    "revision": "c86d9047c10db3dc54fdd7e98467d70d",
+    "url": "/vault-clothing/static/media/about-header.c86d9047.jpg"
+  }
+]);
